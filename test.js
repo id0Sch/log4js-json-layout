bcf3628b77ea198d0af7f9f157688ecc1f25aef2
Ido Schachter <ido.schachter@ironsrc.com>
2016-05-08 13:36:17 +0300

    added source support for layout

--- test.js
+++ test.js
@@ -8,49 +8,40 @@ chai.should();
 
 var layout = require('./lib/jsonLayout');
 
-
+var expected = {
+    startTime: "615 Ludlam Place, Nicholson, New Mexico, 5763",
+    categoryName: "572efdaaa64be9dbc56369ae",
+    data: "Deserunt mollit nisi ipsum ipsum ea quis in eiusmod ipsum officia labore qui amet. Cupidatat ut do Lorem ad veniam irure proident enim aliqua nisi aliquip velit voluptate. Laborum minim occaecat commodo nulla labore ex ullamco. Eu incididunt quis quis Lorem do nostrud enim consectetur. Voluptate in occaecat proident aliqua Lorem pariatur officia dolor.\r\n",
+    level: "strong"
+};
 describe('log4js-json-layout', function () {
     var data;
     beforeEach(function () {
         data = {
-            "_id": "572efdaaa64be9dbc56369ae",
-            "index": 0,
-            "guid": "7091cb87-73d3-477a-ad7f-aca51fc96a2f",
-            "isActive": false,
-            "balance": "$1,262.05",
-            "picture": "http://placehold.it/32x32",
-            "age": 28,
-            "eyeColor": "brown",
-            "name": "Olivia Colon",
-            "gender": "female",
-            "company": "INRT",
-            "email": "oliviacolon@inrt.com",
-            "phone": "+1 (983) 583-2652",
-            "address": "615 Ludlam Place, Nicholson, New Mexico, 5763",
-            "about": "Deserunt mollit nisi ipsum ipsum ea quis in eiusmod ipsum officia labore qui amet. Cupidatat ut do Lorem ad veniam irure proident enim aliqua nisi aliquip velit voluptate. Laborum minim occaecat commodo nulla labore ex ullamco. Eu incididunt quis quis Lorem do nostrud enim consectetur. Voluptate in occaecat proident aliqua Lorem pariatur officia dolor.\r\n"
+            "level": {"levelStr": "strong"},
+            "categoryName": "572efdaaa64be9dbc56369ae",
+            "startTime": "615 Ludlam Place, Nicholson, New Mexico, 5763",
+            "data": "Deserunt mollit nisi ipsum ipsum ea quis in eiusmod ipsum officia labore qui amet. Cupidatat ut do Lorem ad veniam irure proident enim aliqua nisi aliquip velit voluptate. Laborum minim occaecat commodo nulla labore ex ullamco. Eu incididunt quis quis Lorem do nostrud enim consectetur. Voluptate in occaecat proident aliqua Lorem pariatur officia dolor.\r\n"
         };
     });
 
     it('should return all items', function () {
         var output = layout({})(data);
-        output.should.be.deep.equal(JSON.stringify(data));
+        output.should.be.deep.equal(JSON.stringify(expected));
     });
     it('should remove logger property', function () {
         data.logger = "bla,bla,bla";
         var output = layout({})(data);
-        output.should.be.deep.equal(JSON.stringify(data));
+        output.should.be.deep.equal(JSON.stringify(expected));
     });
     it('should change levelStr to level', function () {
-        data.level = {
-            levelStr: 'myLevel'
-        };
         var output = layout({})(data);
-        output.should.be.deep.equal(JSON.stringify(data));
+        output.should.be.deep.equal(JSON.stringify(expected));
     });
     it('should pick specific keys', function () {
         var output = layout({
-            include: ['about', 'address']
+            include: ['level', 'data']
         })(data);
-        output.should.be.deep.equal(JSON.stringify({about: data.about, address: data.address}));
+        output.should.be.deep.equal(JSON.stringify({level: expected.level, data: expected.data}));
     })
 });
